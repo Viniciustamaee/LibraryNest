4baae76934a84a680f33c96318f931538d0f7c0b
PEdbot <vinicius.tamae@pedbot.com.br>
2024-04-16 15:52:47 -0300

    Projeto estruturado

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { BooksModule } from './books/books.module';
+import { RentsModule } from './rents/rents.module';
+import { AuthorsModule } from './authors/authors.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, BooksModule, RentsModule, AuthorsModule, ReviewsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
